433b51ec8f51fee3123fac082b2741793d1ecbea
zaryabriaz <zaryabriaz@ucp.edu.pk>
2019-01-18 23:57:40 +0500

    new work with db and other folders

---
+++ connection/db_connection.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zaryab
+ * Date: 1/18/2019
+ * Time: 11:55 PM
+ */
+
+$con = mysqli_connect("localhost","root","","lhrgaddi");
+if(!$con)
+    die("Connection failed");
